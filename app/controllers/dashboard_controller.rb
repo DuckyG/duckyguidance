d0f8829fe25f7f30664f84b056a63faa7cb791d8
Dan McClain <git@danmcclain.net>
2010-08-02 09:40:21 -0400

    Cleaning up look

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
+  before_filter :require_counselor
   def index
     @meetings = Meeting.where(['counselor_id = ?',current_counselor.id]).order('occured_on desc').limit(10)
     @title = 'Dashboard'
