be3c0093cc687cd9fb27c56071a0db7eb1130215
Dan McClain <git@danmcclain.net>
2010-08-25 08:21:08 -0400

    Trying to fix login issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,11 @@ Guidance::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_controller.session = {:domain => '.viridianspark.com'}
+  if ActionController::Base.session
+    ActionController::Base.session[:domain] = '.viridianspark.com'
+  else
+    ActionController::Base.session = { :domain => '.viridianspark.com' }
+  end
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
