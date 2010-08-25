ba2462d47be1140f02edfce12d15b0d9c4cace8e
Dan McClain <git@danmcclain.net>
2010-08-25 08:38:24 -0400

    Trying to fix login issues

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, :key => '_guidance_session'
+Rails.application.config.session_store :cookie_store, :key => '_guidance_session',:domain => ".viridianspark.com"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
