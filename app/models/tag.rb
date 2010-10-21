fdf9dc6a8e7f1a28d0bf2d8dd1e79d54803285ad
Dan McClain <git@danmcclain.net>
2010-10-21 13:04:43 -0400

    Added uniques constraint on tag by school id

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,5 @@ class Tag < ActiveRecord::Base
   has_many :meeting_tags
   has_many :meetings, :through => :meeting_tags
   belongs_to :school
+  validates_uniqueness_of :name, :scope => :school_id
 end
