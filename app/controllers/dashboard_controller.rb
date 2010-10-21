82f042294fc1f1fbe5e5a1081ad3074f17651da8
Dan McClain <git@danmcclain.net>
2010-10-21 14:54:50 -0400

    Updated dashboard to pull meetings, requests, and upcoming request from the current counselor

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class DashboardController < ApplicationController
   before_filter :require_counselor
   def index
-    @meetings = Meeting.where(['counselor_id = ?',current_counselor.id]).order('occured_on desc').limit(10)
-    @requests = MeetingRequest.where(['counselor_id = ? AND accepted IS NULL',current_counselor.id])
-    @upcoming_meetings = MeetingRequest.where(['counselor_id = ? AND accepted = ? AND desired_date > ?',current_counselor.id,true, DateTime.now]).order('desired_date')
+    @meetings = current_counselor.meetings.order('occured_on desc').limit(10)
+    @requests = current_counselor.meeting_requests.where(['accepted IS NULL'])
+    @upcoming_meetings = current_counselor.meeting_requests.where([' accepted = ? AND desired_date > ?',true, DateTime.now]).order('desired_date')
 
     @title = 'Dashboard'
   end
