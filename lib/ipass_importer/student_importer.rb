b03176bd21c0fc46aa1349b1dc7b85d8a8300995
Dan McClain <git@danmcclain.net>
2010-10-27 10:00:20 -0400

    fixing import script

--- lib/ipass_importer/student_importer.rb
+++ lib/ipass_importer/student_importer.rb
@@ -4,9 +4,10 @@ module StudentImporter
     CSV.foreach(file_path, :headers => true) do |row|
       row_hash = row.to_hash
       counselor_last_name = row_hash.delete('Counselor')
-      counselor = Conselor.find_by_last_name counselor_last_name
+      counselor = school.find_by_last_name counselor_last_name
       new_student = Student.new row_hash
       new_student.counselor = counselor
+      new_student.school = school
       new_student.save
     end
   end
