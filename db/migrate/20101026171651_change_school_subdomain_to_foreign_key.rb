652e0690347f549b3c5c4d4188c7fef3976ae82e
Dan McClain <git@danmcclain.net>
2010-10-27 09:08:28 -0400

    Fixing migration for psql

--- db/migrate/20101026171651_change_school_subdomain_to_foreign_key.rb
+++ db/migrate/20101026171651_change_school_subdomain_to_foreign_key.rb
@@ -1,13 +1,14 @@
 class ChangeSchoolSubdomainToForeignKey < ActiveRecord::Migration
   def self.up
-    change_column :schools, :subdomain, :int
-    rename_column :schools, :subdomain, :subdomain_id
+    remove_column :schools, :subdomain
+    add_column :schools, :subdomain_id, :int
     add_foreign_key :schools, :subdomains
   end
 
   def self.down
-    rename_column :schools, :subdomain_id, :subdomain
-    change_column :schools, :subdomain, :int    
     drop_foreign_key :schools, :subdomains
+    remove_column :schools, :subdomain_id
+    add_column :schools, :subdomain, :string
+    
   end
 end
