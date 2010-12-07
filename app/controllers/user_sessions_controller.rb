87c59d7623966cc09665ef9b8721031d692b9058
Dan McClain <git@danmcclain.net>
2010-12-07 09:41:42 -0500

    Added validations to students model, and fixed the students form to support it.  Fixed labels on students form.  Fixed save method on user sessions controller so that it would error correctly.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -10,8 +10,7 @@ class UserSessionsController < ApplicationController
   def create
     
     @user_session = UserSession.new(params[:user_session])    
-    if @user_session.save!
-
+    if @user_session.save
       if current_subdomain.name == 'admin'
         redirect_to schools_path
       else
