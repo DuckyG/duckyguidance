c3f1e9d9eda4728bab864f76dc7cdda2cb30b01c
Dan McClain <git@danmcclain.net>
2011-03-10 08:18:47 -0500

    Hides students for a counselor that do not belong to the current school.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,7 +7,7 @@ class DashboardController < ApplicationController
     if current_counselor
       #@notes = current_counselor.notes.order('created_at desc').limit(10)
       @notes = Array.new
-      current_counselor.students.all.each {|student| @notes = @notes + student.notes }
+      current_counselor.students.all.each {|student| @notes = @notes + student.notes if student.school == current_school }
       @notes = @notes.uniq {|n| n.id}.sort {|x,y| y.created_at <=> x.created_at}.take(10)
       @requests = current_counselor.meeting_requests.where(['accepted IS NULL']).order('desired_date')
       @upcoming_meetings = current_counselor.meeting_requests.where([' accepted = ? AND desired_date > ?',true, DateTime.now]).order('desired_date').take(10)
