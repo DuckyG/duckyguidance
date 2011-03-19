14151f575608ad3c6cc208254faf51d976f7f325
Dan McClain <git@danmcclain.net>
2011-03-19 10:24:19 -0400

    Adds scope to meeting requests

--- app/controllers/meeting_requests_controller.rb
+++ app/controllers/meeting_requests_controller.rb
@@ -6,7 +6,7 @@ class MeetingRequestsController < ApplicationController
   # GET /meeting_requests
   # GET /meeting_requests.xml
   def index
-    @meeting_requests = current_school.meeting_requests.all
+    @meeting_requests = MeetingRequest.for_school(current_school).all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -17,7 +17,7 @@ class MeetingRequestsController < ApplicationController
   # GET /meeting_requests/1
   # GET /meeting_requests/1.xml
   def show
-    @meeting_request = current_school.meeting_requests.find(params[:id])
+    @meeting_request = MeetingRequest.for_school(current_school).find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -28,7 +28,7 @@ class MeetingRequestsController < ApplicationController
   # GET /meeting_requests/new
   # GET /meeting_requests/new.xml
   def new
-    @meeting_request = MeetingRequest.new
+    @meeting_request = MeetingRequest.for_school(current_school).build
     @title = 'Request a Meeting'
     respond_to do |format|
       format.html # new.html.erb
@@ -41,9 +41,8 @@ class MeetingRequestsController < ApplicationController
   end
   
   def welcome_submit
-    @meeting_request = MeetingRequest.new(params[:meeting_request])
-    @meeting_request.desired_date = Time.strptime(@meeting_request.date + ' ' + @meeting_request.time, "%m/%d/%Y %I:%M %p") if !@meeting_request.date.empty?
-    @meeting_request.school = current_school
+    @meeting_request = MeetingRequest.for_school(current_school).build(params[:meeting_request])
+    
     respond_to do |format|
       if @meeting_request.save
         Notifier.delay.request_submitted(@meeting_request)
@@ -58,14 +57,13 @@ class MeetingRequestsController < ApplicationController
   end
   # GET /meeting_requests/1/edit
   def edit
-    @meeting_request = current_school.meeting_requests.find(params[:id])
+    @meeting_request = MeetingRequest.current_school.find(params[:id])
   end
 
   # POST /meeting_requests
   # POST /meeting_requests.xml
   def create
     @meeting_request = MeetingRequest.new(params[:meeting_request])
-    @meeting_request.desired_date = Time.strptime(@meeting_request.date + ' ' + @meeting_request.time, "%m/%d/%Y %I:%M %p") if !@meeting_request.date.empty?
     @meeting_request.school = current_school
     respond_to do |format|
       if @meeting_request.save
@@ -91,7 +89,7 @@ class MeetingRequestsController < ApplicationController
       params[:meeting_request][:desired_date] = Time.strptime(params[:meeting_request][:date] + ' ' + params[:meeting_request][:time], "%m/%d/%Y %I:%M %p")
       updated = true
     end
-    
+    logger.info @meeting_request.methods.sort
     respond_to do |format|
       if @meeting_request.update_attributes(params[:meeting_request])
         #format.html { redirect_to(@meeting_request, :notice => 'Your meeting request was received.  You should receive an email from one of the counselors shortly.') }
--- app/models/meeting_request.rb
+++ app/models/meeting_request.rb
@@ -1,6 +1,26 @@
+require 'forschoolscope'
 class MeetingRequest < ActiveRecord::Base
+
   belongs_to :counselor
   belongs_to :school
   attr_accessor :date, :time
-  validates :first_name, :last_name, :counselor, :email, :date, :time, :presence => true 
+  validate :check_date
+  before_validation :aggregate_desired_date
+  validates :first_name, :last_name, :counselor, :email, :presence => true
+  
+  scope :for_school, lambda { |current_school| where(:school_id => current_school.id) }
+
+  def check_date
+    Rails::logger.info self
+    unless desired_date
+      errors.add_to_base "Date and Time are required"
+    end
+  end
+  
+  def aggregate_desired_date
+    unless self.date.empty? && self.time.empty?
+      self.desired_date = Time.strptime("#{self.date} #{self.time}" , "%m/%d/%Y %I:%M %p") 
+    end
+  end
+  
 end
