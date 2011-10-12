36cf1c91b83bbfb4c0c5c8224336808778b9d4e9
Dan McClain <git@danmcclain.net>
2011-10-12 10:38:14 -0400

    Implements new dashboard view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,20 +4,26 @@ class DashboardController < ApplicationController
     allow :superadmin
   end
   def index
+
+    @view = params[:view]
+
+    @limit = @view == "list" ? 25 :10
     if current_counselor
-      #@notes = current_counselor.notes.order('created_at desc').limit(10)
       if current_counselor.has_role?(:school_admin, current_school)
-        @notes = current_school.notes.limit(10)
-        logger.info @notes.to_sql
+        @notes = current_school.notes.page(params[:note_page]).per(@limit)
       else
-        @notes = Array.new
-        current_counselor.students.all.each {|student| @notes = @notes + student.notes if student.school == current_school }
-        @notes = @notes.uniq {|n| n.id}.sort {|x,y| y.created_at <=> x.created_at}.take(10)
+        note_ids = NotesStudent.where(student_id: current_counselor.student_ids).select(:note_id).map {|n| n.note_id }
+        @notes = current_school.notes.where(id: note_ids).limit(@limit).page(params[:note_page]).per(@limit)
       end
-      @requests = current_counselor.meeting_requests.where(['accepted IS NULL']).order('desired_date')
-      @upcoming_meetings = current_counselor.meeting_requests.accepted.future.order('desired_date').take(10)
     end
-    @section = 'Dashboard'
+
+
+
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
 end
