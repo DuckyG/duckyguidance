3dff3da97a78728f164619e674269ea17b7a8b86
Dan McClain <git@danmcclain.net>
2011-11-17 09:11:38 -0500

    Fixes older migration

--- db/migrate/20101201171904_add_show_tags_to_schools.rb
+++ db/migrate/20101201171904_add_show_tags_to_schools.rb
@@ -2,10 +2,6 @@ class AddShowTagsToSchools < ActiveRecord::Migration
   def self.up
     add_column :schools, :show_tags, :boolean, :default => true
 
-    School.each do |s|
-      s.show_tags = true
-      s.save
-    end
   end
 
   def self.down
