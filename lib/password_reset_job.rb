6061c4147441c2b18bc58e0c6d8c6d17ad433494
Dan McClain <git@danmcclain.net>
2011-12-21 07:43:48 -0500

    Trying to fix reset password mailer

--- lib/password_reset_job.rb
+++ lib/password_reset_job.rb
@@ -1,8 +1,11 @@
+require File.expand_path(File.dirname(__FILE__) + "/../config/initializers/devise.rb")
+
 class PasswordResetJob < Struct.new(:id)
   def perform
     counselor = Counselor.find(id)
     ActionMailer::Base.default_url_options[:protocol] = "https"
     ActionMailer::Base.default_url_options[:host] = "#{counselor.school.subdomain.name}.duckyg.com"
+    Devise::Mailer.
     Devise::Mailer.reset_password_instructions(counselor).deliver
   end
 end
