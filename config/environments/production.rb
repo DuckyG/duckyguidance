9ed075bb76d00c110e6756192130fa23d75b325e
Dan McClain <git@danmcclain.net>
2013-01-13 07:42:31 -0500

    Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,4 +56,13 @@ Guidance::Application.configure do
    :email_prefix => "[DuckyG Error] ",
    :sender_address => %{"notifier" <notifier@duckyg.com>},
    :exception_recipients => %w{duckyg@danmcclain.net}
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 end
